03d65cf49eb7d3dcea3113928a5ee37d8dd724bd
Pooja Sanap <88768476+pooja-sanap-53@users.noreply.github.com>
2022-11-07 00:11:32 +0530

    Time: 2 ms (52.87%), Space: 41.9 MB (69.40%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String interpret(String command) {
+        command = command.replace("()", "o");
+        command = command.replace("(al)", "al");
+        
+        return command;
+    }
+}
