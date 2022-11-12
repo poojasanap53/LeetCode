a5b2d133470d811ccf071321f4abfebaa9db4eea
Pooja Sanap <88768476+pooja-sanap-53@users.noreply.github.com>
2022-11-12 23:56:41 +0530

    Time: 1 ms (49.60%), Space: 42.2 MB (58.92%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
@@ -1,9 +1,24 @@
 class Solution {
     public int[] sumZero(int n) {
         int[] ans = new int[n];
+        if (n == 1){
+            ans[0] = 0;
+        }
+        
+        else if(n % 2 ==0){
             for(int i = 0; i<n; i++){
                 ans[i] = i * 2 - n + 1; 
             }
+        }
+        
+        else if(n % 2 != 0 ){
+            int a = n / 2;
+            for(int i = 0; i<n; i++){
+                ans[i] = -a + i; 
+                
+            }
+        }
+        
         
         return ans;
     }
