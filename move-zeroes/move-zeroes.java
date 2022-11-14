0e2656bd91bfb9c57a65c62e69c17bfb6ef42322
Pooja Sanap <88768476+pooja-sanap-53@users.noreply.github.com>
2022-11-14 23:57:42 +0530

    Time: 1 ms, Memory: 43.4 MB - LeetHub

---
+++ move-zeroes/move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int p = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i] != 0){
+                nums[p] = nums[i];
+                p++;
+            }
+        }   
+        for(int i = p; i<nums.length; i++){
+            nums[i] =0;
+        }
+        
+    }
+}
