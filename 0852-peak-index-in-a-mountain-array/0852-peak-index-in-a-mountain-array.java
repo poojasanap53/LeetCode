323422fefd36772b74f78bafee89d2ebfbac8135
Pooja Sanap <88768476+pooja-sanap-53@users.noreply.github.com>
2022-11-29 15:07:45 +0530

    Time: 0 ms (100.00%), Space: 59.5 MB (85.49%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,16 +1,19 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        //brute force solution --O(n) time complexity
-        int max = arr[0];
-        int ans = -1;
-        for(int i = 0 ; i < arr.length ; i++){
-            if(arr[i] > max){
-                max = arr[i];
-                ans = i;
-            }
-            
-        }
+        // binary search solution
         
-        return ans;
+        int start = 0;
+        int end = arr.length - 1;
+        while(start < end){
+            int mid = start + (end - start)/2;
+
+            if(arr[mid] > arr[mid + 1]){
+                end = mid;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return start ;
     }
 }
