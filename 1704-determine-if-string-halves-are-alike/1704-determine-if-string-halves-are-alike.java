4f55548e23c6389710886381ef9508de76f91eb3
Pooja Sanap <88768476+pooja-sanap-53@users.noreply.github.com>
2022-12-02 00:08:09 +0530

    Time: 17 ms (16.80%), Space: 42.8 MB (21.71%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        int cta = 0;
+        int ctb = 0;
+        for(int i = 0; i <s.length()/2; i++){
+             if(s.charAt(i) == 'a'|| s.charAt(i) =='e'|| s.charAt(i) == 'i'|| s.charAt(i) =='o'|| s.charAt(i)== 'u' || s.charAt(i) =='A' || s.charAt(i) == 'E' || s.charAt(i) =='I' || s.charAt(i) == 'O' || s.charAt(i) =='U')
+             {
+                cta++;
+            }
+        }
+        
+        for(int i = s.length()/2; i <s.length(); i++){
+            if(s.charAt(i) == 'a' || s.charAt(i) =='e' || s.charAt(i) == 'i' || s.charAt(i) =='o' || s.charAt(i)== 'u' || s.charAt(i) =='A' || s.charAt(i) == 'E' || s.charAt(i) =='I' || s.charAt(i) == 'O' || s.charAt(i) =='U')
+            {
+                ctb++;
+            }
+        }
+        
+        return cta==ctb;
+    }
+}
